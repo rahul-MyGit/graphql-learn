082dcbf8f6bffc9f037eab2b0aa1e1f03186da4c
Rahul <rahul.mymail1@gmail.com>
2025-05-29 17:40:25 +0530

    feat: basic apolloServer

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,40 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@as-integrations/express5";
 import express from "express";
 
-const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World 2');
-});
+async function init() {
+  const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+  app.use(express.json());
+
+  const server = new ApolloServer({
+    typeDefs: `
+      type Query {
+        hello: String
+        say(name: String): String
+      }
+    `, //schema
+    resolvers: {
+      Query: {
+        hello: () => 'Hello World from resolver',
+        say: (_, {name}: {name: string}) =>  `Hi Big Boy ${name}. See you later`
+      }
+    }, //actual function of query and mutation
+  });
+
+  await server.start();
+
+  app.get('/', (req, res) => {
+    res.send('Hello World 2');
+  });
+
+  app.use('/graphql', expressMiddleware(server));
+
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+
+}
+
+init();
