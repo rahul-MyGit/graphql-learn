315c601cbb0f2f70ad0a4cabda6f339ec8c15c29
Rahul <rahul.mymail1@gmail.com>
2025-05-31 13:21:18 +0530

    feat: query

--- backend/src/model/book.model.ts
+++ backend/src/model/book.model.ts
@@ -26,12 +26,38 @@ export const BookTypeDef = gql`
         publisher: String
     }
 
+    extend type Query {
+        bookList: [Book]!
+        book(id: ID!): Book!
+    }
+
     extend type Mutation {
         bookCreate(input: BookInput!): Book
     }
 `
 
 export const BookResolver = {
+    Query: {
+        bookList: async (_: any, __: any, {req}: GraphContext) => {
+            authMiddlewareGraphql(req);
+            const books = await prisma.booking.findMany({
+                where: {
+                    userId: req.user?.id
+                },
+                orderBy: {
+                    createdAt: 'desc'
+                }
+            });
+            return books;
+        },
+        book: async (_: any, {id}: any, {req}: GraphContext) => {
+            authMiddlewareGraphql(req);
+            const book = await prisma.booking.findUnique({
+                where: { id }
+            });
+            return book;
+        }
+    },
     Mutation: {
         bookCreate: async (_: any, {input}: any, {req}: GraphContext) => {
             authMiddlewareGraphql(req);
